88f973938aef001c009a2881298231a0a5a96293
WAHYU ADI SURYANTO <wasuryanto3@gmail.com>
2024-08-03 18:26:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam Nur Kholifah", "Jangan Kaget yaaa", "I Love You IPEEEHHHHH :D"],
+        typeSpeed: 70
+    });
+}
