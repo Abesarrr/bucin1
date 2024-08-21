764e0af97556e4c106f9cb4626fcfa2ab79e76b4
WAHYU ADI SURYANTO <wasuryanto3@gmail.com>
2024-08-21 21:26:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Cuma mau bilang", "Selamat Malam Anggi", "Jangan Begadang Yaaa", "❤️❤️❤️❤️❤️"],
+        strings: ["Cuma mau bilang", "Selamat Malam Anggita", "Jangan Begadang Yaaa", "❤️❤️❤️❤️❤️"],
         typeSpeed: 70
     });
 }
